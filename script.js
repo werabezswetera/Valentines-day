833ad0e5dfb20069df5c5785225766c845573a16
werabezswetera <weronikazawitkowska@gmail.com>
2025-02-11 20:53:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function selectOption(option) {
             displayCatHeart(); // Display the cat-heart.gif
         });
     } else if (option === 'no') {
-        // Change text on the "No" button to "You BITCH."
-        document.getElementById('no-button').innerText = 'You BITCH.'; 
+        // Change text on the "No" button to "You bitch."
+        document.getElementById('no-button').innerText = 'You bitch.'; 
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
